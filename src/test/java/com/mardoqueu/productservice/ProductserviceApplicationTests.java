c6aac725c7c6e684131ce381221ae3bdda13d8be
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-21 20:02:52 -0300

    setup initial project

---
+++ src/test/java/com/mardoqueu/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardoqueu.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
